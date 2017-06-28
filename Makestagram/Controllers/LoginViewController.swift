2c112018cba9987bcaea08eb8319389cac67135b
smo202 <jerrylu202@gmail.com>
2017-06-28 13:52:56 -0400

    implement login flow
    installed dependencies
    connected to firebase
    completed basic setup
    implemented login flow

---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Jerry Lu on 6/28/17.
+//  Copyright © 2017 Jerry Lu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("login button tapped")
+    }
+    
+    
+}
