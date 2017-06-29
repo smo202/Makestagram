662fc40e5d9bc68de7d5a091f61abf4f042fdf49
smo202 <jerrylu202@gmail.com>
2017-06-29 10:56:04 -0400

    handle existing users
    implemented logic to handle existing users

--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
+//let user: FIRUser? = Auth.auth().currentUser
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
@@ -18,8 +24,41 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-        print("login button tapped")
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
     }
     
     
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        // 3
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                print("Welcome back, \(user.username).")
+            } else {
+                print("New user!")
+            }
+        })
+    }
 }
---
+++ Makestagram/Models/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  Makestagram
+//
+//  Created by Jerry Lu on 6/29/17.
+//  Copyright © 2017 Jerry Lu. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase.FIRDataSnapshot
+
+class User {
+    
+    // MARK: - Properties
+    
+    let uid: String
+    let username: String
+    
+    // MARK: - Init
+    
+    init(uid: String, username: String) {
+        self.uid = uid
+        self.username = username
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard let dict = snapshot.value as? [String : Any],
+            let username = dict["username"] as? String
+            else { return nil }
+        
+        self.uid = snapshot.key
+        self.username = username
+    }
+}
